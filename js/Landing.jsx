3ad8122cb6c50ed5e10731000c03690c100ff299
Eric Murithi <murithier.2006@gmail.com>
2017-09-21 11:58:00 +0300

    Completed Show card

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,9 +1,10 @@
+// @flow
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Landing = () => (
   <div className="landing">
-    <h1>svideo</h1>
+    <h1>myvideo</h1>
     <input type="text" placeholder="Search" />
     <Link to="/search">or Browse All</Link>
   </div>
--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import { string } from 'prop-types';
+// @flow
+import React, { Component } from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Wrapper = styled.div`
+const Wrapper = styled(Link)`
   width: 32%;
   border: 2px solid #333;
   border-radius: 4px;
   margin-bottom: 25px;
   padding-right: 10px;
   overflow: hidden;
+  color:black;
+  text-decoration: none;
 `;
 
 const Image = styled.img`
@@ -16,23 +19,24 @@ const Image = styled.img`
   float: left;
   margin-right: 10px;
 `;
+class ShowCard extends Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+  props: Show;
+  render() {
+    return (
+      <Wrapper to={`/details/${this.props.imdbID}`}>
 
-const ShowCard = props => (
-  <Wrapper>
-    <Image alt={`${props.title} Show Poster`} src={`/public/img/posters/${props.poster}`} />
-    <div>
-      <h3>{props.title}</h3>
-      <h4>({props.year})</h4>
-      <p>{props.description}</p>
-    </div>
-  </Wrapper>
-);
-
-ShowCard.propTypes = {
-  poster: string.isRequired,
-  title: string.isRequired,
-  year: string.isRequired,
-  description: string.isRequired
-};
+        <Image alt={`${this.props.title} Show Poster`} src={`/public/img/posters/${this.props.poster}`} />
+        <div>
+          <h3>{this.props.title}</h3>
+          <h4>({this.props.year})</h4>
+          <p>{this.props.description}</p>
+        </div>
+      </Wrapper>
+    );
+  }
+}
 
 export default ShowCard;
